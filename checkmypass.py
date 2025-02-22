fa55a8f810a70aff91b3da73bd0bd693343616a5
Bilgekaan36 <b.yilmaz@workdigital.de>
2025-02-22 23:16:52 +0300

    add password leaks count and import sys lib

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -9,13 +10,26 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
     return res
 
-def read_res(response):
-    print(response.text)
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count     
+    return 0   
 
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
-    return read_res(response)    
+    return get_password_leaks_count(response, tail)    
 
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times... you should probably change your password')
+        else:
+            print(f'{password} was NOT found. Carry on!')
+    return 'done!'
+
+main(sys.argv[1:])
